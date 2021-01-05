6d3ab3d976e12f1181892cad9b5f0766b28ec636
Quentin Millardet <quentin.millardet@telecomnancy.eu>
2021-01-05 22:27:19 +0100

    Ajout du parse CSV

--- Projet STER/MainPage.xaml.cpp
+++ Projet STER/MainPage.xaml.cpp
@@ -2,12 +2,18 @@
 // MainPage.xaml.cpp
 // Implémentation de la classe MainPage.
 //
-
+#define _CRT_SECURE_NO_DEPRECATE
+#include <stdio.h>
 #include "pch.h"
 #include "MainPage.xaml.h"
 #include <thread>
-//#include <mutex>
 #include <shared_mutex>
+#include <string>
+#include <sstream>
+#include <vector>
+#include <iterator>
+#include <locale> 
+#include <codecvt>
 
 
 using namespace Projet_STER;
@@ -27,6 +33,8 @@ using namespace Windows::UI::Xaml::Input;
 using namespace Windows::UI::Xaml::Media;
 using namespace Windows::UI::Xaml::Navigation;
 using namespace Windows::Data::Json;
+using namespace Windows::Storage;
+using namespace Windows::Storage::Streams;
 
 using namespace std;
 // Pour plus d'informations sur le modèle d'élément Page vierge, consultez la page https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
@@ -51,6 +59,14 @@ Windows::Foundation::Uri^ uri = ref new Uri(url);
 HANDLE Verrou;
 shared_mutex Ver;
 
+struct MoteInfoCSV {
+	Platform::String^ numMote;
+	Platform::String^ nomMote;
+	float latitude;
+	float longitude;
+};
+
+std::vector<MoteInfoCSV> listOfMote;
 
 static UINT Inc()
 {
@@ -131,6 +147,8 @@ void Projet_STER::MainPage::Button_Click(Platform::Object^ sender, Windows::UI::
 	// Amphis Nord
 	nomMote = "9.138"; 
 	Ver.unlock();
+	string s = "9.138";
+	//parseCSV(s, L"Google");
 }
 
 void Projet_STER::MainPage::OnTick(Object ^ sender, Object ^ e) {
@@ -160,4 +178,76 @@ Platform::String^ Projet_STER::MainPage::recupererDonneesMote(JsonObject^ objJso
 		}
 	}
 	return res;
+}
+
+
+/*
+	Permet d'extraire les données de lumière des données reçu d'une mote
+*/
+Platform::String^ Projet_STER::MainPage::parseCSV(std::string nomMote, Platform::String^ elmenet)
+{
+	
+
+	/*FILE *fp;
+	fp = fopen("C:\\motes_information.csv", "r");
+
+	StorageFolder^ storageFolder = ApplicationData::Current->LocalFolder;
+	create_task(storageFolder->GetFileAsync("sample.txt")).then([](StorageFile^ sampleFile)
+	{
+		float latitude, logitude;
+	string numMote, nomMoteGet;
+		while (fscanf(fp, "%g,%g,%s,%s\n", &latitude, &logitude, &numMote, &nomMoteGet) == 2)
+		{
+			if (numMote == "9.138") {
+				OutputDebugString(L"Trouvé");
+			}
+		}
+	});*/
+	StorageFolder^ storageFolder = ApplicationData::Current->LocalFolder;
+	create_task(storageFolder->GetFileAsync("motes_information.csv")).then([](StorageFile^ sampleFile)
+	{
+		return FileIO::ReadBufferAsync(sampleFile);
+
+	}).then([this](Streams::IBuffer^ buffer)
+	{
+		auto dataReader = DataReader::FromBuffer(buffer);
+		String^ bufferText = dataReader->ReadString(buffer->Length);
+		wstring csvFile = bufferText->Data();
+		float latitude, logitude;
+		string numMote, nomMoteGet;
+		using convert_type = std::codecvt_utf8<wchar_t>;
+		std::wstring_convert<convert_type, wchar_t> converter;
+		std::string s = converter.to_bytes(csvFile);
+		std::string delimiter = "\n";
+
+		size_t pos = 0;
+		std::string token;
+		std::string csvDelimiter = ",";
+		while ((pos = s.find(delimiter)) != std::string::npos) {
+
+			token = s.substr(0, pos);
+			std::vector<std::string> res = split(token, csvDelimiter);
+			MoteInfoCSV info;
+			info.nomMote = ref new String(converter.from_bytes(res[3].c_str()).c_str());
+			info.numMote = ref new String(converter.from_bytes(res[2].c_str()).c_str());
+			info.latitude = std::stod(res[0]);
+			info.longitude = std::stod(res[1]);
+			listOfMote.push_back(info);
+			s.erase(0, pos + delimiter.length());
+		}
+	});
+	
+	return L"9.138";
+}
+
+std::vector<std::string> Projet_STER::MainPage::split(std::string str, std::string sep) {
+	char* cstr = const_cast<char*>(str.c_str());
+	char* current;
+	std::vector<std::string> arr;
+	current = strtok_s(cstr, sep.c_str(), NULL);
+	while (current != NULL) {
+		arr.push_back(current);
+		current = strtok_s(NULL, sep.c_str(), NULL);
+	}
+	return arr;
 }
--- Projet STER/MainPage.xaml.h
+++ Projet STER/MainPage.xaml.h
@@ -23,6 +23,8 @@ namespace Projet_STER
 		void OnTick(Object ^ sender, Object ^ e);
 		void AfficherInfo(Platform::String^ nomMote);
 		Platform::String^ recupererDonneesMote(Windows::Data::Json::JsonObject^ objJson, Platform::String^ label);
+		Platform::String ^ parseCSV(std::string nomMote, Platform::String ^ elmenet);
+		std::vector<std::string> split(std::string str, std::string sep);
 		bool isFilterUsed;
 		Windows::Web::Http::Filters::HttpBaseProtocolFilter^ filter;
 		Windows::Web::Http::HttpClient^ httpClient;
